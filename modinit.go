c8f3aca02d29fa1a10b0b787f82c7c20793ffd13
0w0mewo <72908579+0w0mewo@users.noreply.github.com>
2022-06-07 08:39:42 +0000

    move modules init to spearted source file

---
+++ modinit.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	_ "github.com/0w0mewo/mcrc_tgbot/bot/modules/commands"
+	_ "github.com/0w0mewo/mcrc_tgbot/bot/modules/mlcapi"
+	_ "github.com/0w0mewo/mcrc_tgbot/bot/modules/repeater"
+	_ "github.com/0w0mewo/mcrc_tgbot/bot/modules/tweetforward"
+)
